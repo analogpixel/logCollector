f6af09218a7415889df2ce69853963b30cd3435b
Matt Poepping <matt.poepping@onvoy.com>
2017-09-13 10:16:37 -0600

    before change

--- static/js/app.js
+++ static/js/app.js
@@ -1,1 +1,35 @@
+/*
+https://vuejs.org/v2/guide/components.html
+https://vuejs.org/v2/guide/forms.html
+*/
 
+
+Vue.component('mattbox' , {
+	template: '\
+	<div id="mattbox-template" v-on:click="inc">hello <p>{{ mattnew }}</p> {{ counter }}\
+	<select v-model="selected" name="sometext" size="5">\
+	 <option v-for="option in items" v-bind:value="option.value">\
+    {{ option.text }}\
+  	</option>\
+	  </select><span>Selected: {{ selected }}</span></div>',
+	
+	data: function() {return {
+		mattnew: "sdfsddsfs",
+		items: [
+		{text: 'one', value: 'a'},
+		{text: 'two', value: 'abb'}],
+		counter: 0,
+		selected: 'a',
+	}},
+
+	methods: {
+		inc: function(event) {  this.counter = this.counter + 1; this.mattnew = "aaaaa"}
+	}
+
+});
+
+$(function() {
+	var app1 = new Vue({
+	  el: '#app'
+	});
+});
--- templates/client.sh
+++ templates/client.sh
@@ -6,8 +6,9 @@ eval `curl -s {{ serverUrl }}client/jobs/$hostname`
 for files in "${logs[@]}"
 do
 
-	filenames=${files%:*}
-	projectID=${files#*:}
+	filename=$(echo $files | cut -f1 -d:)
+	projectID=$(echo $files | cut -f2 -d:)
+
 	# echo "files: $filenames projectID: $projectID"
 	for file in $filenames
 		do
